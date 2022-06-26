8ce0614ee506b0fee4d7edf34a2aac0381ebf6bd
heineiuo <heineiuo@gmail.com>
2022-06-26 22:39:58 +0800

    Add form

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import http2 from "http2";
 import tls from "tls";
 import fs from "fs";
+import { Readable } from "stream";
 
 function SNICallback(servername, cb) {
   cb(
@@ -18,14 +19,46 @@ const server = http2
       SNICallback,
       allowHTTP1: true,
     },
-    (req, res) => {
-      if (req.url === "/") {
+    async (req, res) => {
+      console.log(req.method, req.url);
+      req.once("aborted", () => {
+        console.log(`req aborted`);
+      });
+      res.once("aborted", () => {
+        console.log(`res aborted`);
+      });
+      if (req.url.startsWith("/action") && req.method === 'POST') {
+        const stream = Readable.toWeb(req);
+        const reader = stream.getReader();
+        async function readChunks() {
+          let chunks = [];
+          while (true) {
+            try {
+              const chunk = await reader.read();
+              console.log(chunk)
+
+              if (chunk.done) {
+                break;
+              }
+              chunks.push(chunk.value);
+            } catch (e) {
+              console.error(e);
+              break;
+            }
+          }
+          return chunks;
+        }
+        const chunks = await readChunks();
+        console.log(chunks);
+        res.end("ok");
+      } else {
         res.end(`<html>
-          <script src="/main.js"></script>
+          <form action="/action" method="POST">
+            <input type="text" name="input" />
+            <button type="submit">submit</button>
+          </form>
       </html>`);
-        return;
       }
-      console.log(req.url);
     }
   )
   .listen(9050, () => {
